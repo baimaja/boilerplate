19094be7fd588e4b34d532aa42b9963c09e45fbb
baim aja <ibrahim442@smk.belajar.id>
2024-08-22 03:35:45 -0700

    boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import React from 'react';
+import { ThemeProvider } from './contexts/ThemeContexts';
+import { UserProvider } from './contexts/UserContexts';
+import { CounterProvider } from './contexts/CounterContext';
+import ThemedComponent from './components/ThemedComponent';
+import UserInfo from './components/UserInfo';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <UserProvider>
+        <CounterProvider>
+          <ThemedComponent />
+          <UserInfo />
+          <Counter />
+        </CounterProvider>
+      </UserProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
